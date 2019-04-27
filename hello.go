ceaa836384f4bbc12935025f97b2bc5f4659bce0
lxq <lxq@weetgo.com>
2019-04-27 20:59:27 +0800

    hello golang.

---
+++ hello.go
@@ -0,0 +1,15 @@
+package main
+
+/**
+ * hello world for Go.
+ * 
+ * @date 2019/4/26
+ * @author Allen Lin
+ * @email xqlin@qq.com
+ */
+
+ import "fmt"
+
+ func main() {
+	fmt.Println("你好， Go.")
+ }
