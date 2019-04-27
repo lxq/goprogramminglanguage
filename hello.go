089cebd42d4702445ba2efd51f2c0dd0d3703303
lxq <lxq@weetgo.com>
2019-04-27 21:28:03 +0800

    os.Args test.

--- hello.go
+++ hello.go
@@ -1,5 +1,3 @@
-package main
-
 /**
  * hello world for Go.
  * 
@@ -7,9 +5,43 @@ package main
  * @author Allen Lin
  * @email xqlin@qq.com
  */
+package main
 
- import "fmt"
+
+import(
+	"fmt"
+	"os"
+)
 
  func main() {
 	fmt.Println("你好， Go.")
+
+	echo1()
+	echo2()
+ }
+
+ /** 
+  * test1 for os.Args .
+ */
+ func echo1() {
+	const sep = " "
+	var str string
+	for i := 0; i < len(os.Args); i++ {
+		str += sep + os.Args[i]
+	}
+	fmt.Println(str)
+ }
+
+ // 采用 for...rage，效率更高。
+ func echo2() {
+	// 短变更声明
+	str, sep := "", ""
+	// _ 为空标识符
+	// for...range 一般用于字符串或Slice上进行迭代.
+	// go 中只有一个循环：for [init;] [condition;] [post] {  }
+	for _, arg := range(os.Args) {
+		str += sep + arg
+		sep = " "
+	}
+	fmt.Println(str)
  }
