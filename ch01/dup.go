5fef1a4b57f8c4f37f64d2168e7f7b5649d281e1
lxq <lxq@weetgo.com>
2019-05-20 14:10:19 +0800

    抽象成函数.

--- ch01/dup.go
+++ ch01/dup.go
@@ -15,21 +15,37 @@ import (
 
 func main() {
 	fmt.Println("UNIX uniq 指令.")
-	dup1()
-}
+	// dup1()
+	res := make(map[string]int)
+	dupCounter(os.Stdin, res)
 
-// 统计标准输入中次数大于1的行.
-func dup1() {
-	counts := make(map[string]int)
-	input := bufio.NewScanner(os.Stdin)
-	for input.Scan() {
-		counts[input.Text()]++
-	}
-
-	// TODO: 忽略input.Err()中可能的错误.
-	for str, n := range counts {
+	for str, n := range res {
 		if n > 1 {
 			fmt.Printf("%d\t%s\n", n, str)
 		}
 	}
 }
+
+func dupCounter(f *os.File, res map[string]int) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		res[input.Text()]++
+	}
+	// TODO: 忽略input.Err()中可能的错误.
+}
+
+// // 统计标准输入中次数大于1的行.
+// func dup1() {
+// 	counts := make(map[string]int)
+// 	input := bufio.NewScanner(os.Stdin)
+// 	for input.Scan() {
+// 		counts[input.Text()]++
+// 	}
+
+// 	// TODO: 忽略input.Err()中可能的错误.
+// 	for str, n := range counts {
+// 		if n > 1 {
+// 			fmt.Printf("%d\t%s\n", n, str)
+// 		}
+// 	}
+// }
