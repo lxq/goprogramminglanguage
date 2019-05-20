8895ca69eaa0562a99f7065f4d96979d8919044e
lxq <lxq@weetgo.com>
2019-05-20 20:35:31 +0800

    ReadFile()版本的dup.

--- ch01/dup.go
+++ ch01/dup.go
@@ -10,7 +10,9 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io/ioutil"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -21,7 +23,7 @@ func main() {
 	fileNames := os.Args[1:]
 	if len(fileNames) == 0 {
 		fmt.Println("未输入文件，采用命令行输入进行统计!")
-		dupCounter(os.Stdin, res)
+		dupStream(os.Stdin, res)
 	} else {
 		for _, arg := range fileNames {
 			f, err := os.Open(arg)
@@ -29,11 +31,14 @@ func main() {
 				fmt.Fprintf(os.Stderr, "dup files: %v\n", err)
 				continue
 			}
-			dupCounter(f, res)
+			dupStream(f, res)
 			f.Close()
+			// read a file into memroy
+			dupFile(arg)
 		}
 	}
 
+	fmt.Println("全部统计结果:")
 	for str, n := range res {
 		if n > 1 {
 			fmt.Printf("%d\t%s\n", n, str)
@@ -42,10 +47,39 @@ func main() {
 }
 
 // 把File、os.Stdin进行统一.
-func dupCounter(f *os.File, res map[string]int) {
+// 这种逐行处理相当于“流”模式.
+func dupStream(f *os.File, res map[string]int) {
 	input := bufio.NewScanner(f)
 	for input.Scan() {
+		// Scanner.Text()返回的结果不包含换行符等.
 		res[input.Text()]++
 	}
 	// TODO: 忽略input.Err()中可能的错误.
+}
+
+/**
+ * 统计单个文件中的重复行次数.
+ * 用于练习ioutil.ReadFile(), string.Split()等方法。
+ */
+func dupFile(filename string) {
+	fmt.Println("统计单个文件中的行重复次数.")
+	// TODO: 加入文件是否存在的判断.
+	res := make(map[string]int)
+	// ReadFile()一次把全部内容读取内存.
+	// 返回值 data 是一个可以转换为字符串的字节slice.
+	// TODO: 返回值为字节slice，是否意味着ReadFile()不能读取二进制文件？
+	data, err := ioutil.ReadFile(filename)
+	if nil != err {
+		fmt.Fprintf(os.Stderr, "%s 读取错误: %v\n", filename, err)
+		return
+	}
+
+	// strings.Split()按给定分隔符把字符串分成slice.
+	for _, line := range strings.Split(string(data), "\n") {
+		// TODO: 这里读取的行内容，并未考虑换行符情况，最行一行内容没有换行则会出现即使其他行内容一致也会统计成不一样.
+		res[line]++
+	}
+	for line, n := range res {
+		fmt.Printf("%d\t%s\n", n, line)
+	}
 }
