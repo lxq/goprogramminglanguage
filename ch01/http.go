9a8fa4b91c197610ec61b6d63468351160bcde22
lxq <lxq@weetgo.com>
2019-05-22 16:35:53 +0800

    sync.Mutex练习.

--- ch01/http.go
+++ ch01/http.go
@@ -11,16 +11,35 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"sync"
 )
+
+// 统计被访问的次数
+var count int
+
+// 互斥量，用于避免并行访问时计数冲突.
+var mu sync.Mutex
 
 func main() {
 	fmt.Println("simple http server.")
 
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/count", counter)
 	log.Fatal(http.ListenAndServe("localhost:8899", nil))
 }
 
 // http的标准路由处理函数，参数列表固定.
 func handler(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	count++
+	mu.Unlock()
+
 	fmt.Fprintf(w, "URL: %q\n", r.URL.Path)
+}
+
+// 计数路由
+func counter(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	fmt.Fprintf(w, "首页被访问次数：%d\n", count)
+	mu.Unlock()
 }
