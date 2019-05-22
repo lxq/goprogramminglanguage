74fa08e2ec552973a764a7466d6116f8b2fdc941
lxq <lxq@weetgo.com>
2019-05-22 09:46:35 +0800

    add comments.

--- ch01/fetch.go
+++ ch01/fetch.go
@@ -17,11 +17,13 @@ import (
 
 func main() {
 	for _, url := range os.Args[1:] {
+		// http.Get()产生一个HTTP请求.
 		res, err := http.Get(url)
 		if nil != err {
 			fmt.Fprintf(os.Stderr, "fetch: %v \n", err)
 			os.Exit(1)
 		}
+		// ReadAll()一次读取全部内容.
 		body, err := ioutil.ReadAll(res.Body)
 		res.Body.Close()
 		if nil != err {
