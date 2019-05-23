b5512bd105e640699a74725ee40644fc0c8aabb9
lxq <lxq@weetgo.com>
2019-05-23 20:10:57 +0800

    调用自定义包.

---
+++ ch02/c2f.go
@@ -0,0 +1,30 @@
+/**
+ * 练习调用自定义的包.
+ * 2.6 节
+ *
+ * @date 2019/5/23
+ * @author Allen Lin
+ * @email xqlin@qq.com
+ */
+package main
+
+import (
+	"fmt"
+	"gopl/ch02/tmpc"
+	"os"
+	"strconv"
+)
+
+func main() {
+	for _, arg := range os.Args[1:] {
+		t, err := strconv.ParseFloat(arg, 64)
+		if nil != err {
+			fmt.Fprintf(os.Stderr, "参数错误:%v\n", err)
+			os.Exit(1)
+		}
+
+		f := tmpc.Fahrenheit(t)
+		c := tmpc.Celsuis(t)
+		fmt.Printf("%s = %s, %s = %s\n", f, tmpc.FtoC(f), c, tmpc.CtoF(c))
+	}
+}
