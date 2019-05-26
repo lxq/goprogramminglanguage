cf5257966fe18d4499d7a09205eea605e375b7ff
lxq <lxq@weetgo.com>
2019-05-26 23:05:54 +0800

    sha256数组比较练习.

---
+++ ch04/sha256.go
@@ -0,0 +1,22 @@
+/**
+ * cypto/sha256包练习数组比较
+ * 4.1 节
+ *
+ * @date 2019/5/26
+ * @author Allen Lin
+ * @email xqlin@qq.com
+ */
+
+package main
+
+import (
+	"crypto/sha256"
+	"fmt"
+)
+
+func main() {
+	fmt.Println("sh256:数组比较")
+	c1 := sha256.Sum256([]byte("x"))
+	c2 := sha256.Sum256([]byte("X"))
+	fmt.Printf("sha256 c1:%x\nsha256 c2:%x\nc1==c2:%t\n%T\n", c1, c2, c1 == c2, c1)
+}
