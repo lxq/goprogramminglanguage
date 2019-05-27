54fd793f725a88b20392d728bd3efdede5acaa85
lxq <lxq@weetgo.com>
2019-05-28 00:15:33 +0800

    增加重要注释.

--- ch04/rev.go
+++ ch04/rev.go
@@ -15,6 +15,7 @@ import (
 
 func main() {
 	fmt.Println("Array & Slice.")
+	// a 是数组, [...]表示长度由初始化值个数确定.
 	a := [...]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 	reverse(a[:])
 	fmt.Println(a)
@@ -22,7 +23,9 @@ func main() {
 
 // reverse a slice
 func reverse(s []int) {
+	// 这里不能用i++,j--，原因是i++,j--是语句，不是表达式
 	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		// for 和下面语句都体现了go的多赋值便利性
 		s[i], s[j] = s[j], s[i]
 	}
 }
