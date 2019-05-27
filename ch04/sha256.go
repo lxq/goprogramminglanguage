a337ed46bb49e35bee58270af551f242df58e0eb
lxq <lxq@weetgo.com>
2019-05-27 09:42:56 +0800

    update comments.

--- ch04/sha256.go
+++ ch04/sha256.go
@@ -18,5 +18,6 @@ func main() {
 	fmt.Println("sh256:数组比较")
 	c1 := sha256.Sum256([]byte("x"))
 	c2 := sha256.Sum256([]byte("X"))
+	// %x ：十六进制， %t:布尔值，%T：值类型
 	fmt.Printf("sha256 c1:%x\nsha256 c2:%x\nc1==c2:%t\n%T\n", c1, c2, c1 == c2, c1)
 }
