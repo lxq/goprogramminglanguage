aea53e61b2ba0c9f64db987f645539bb1bf926ff
lxq <lxq@weetgo.com>
2019-05-29 15:31:59 +0800

    增加字符统计的map练习。

--- ch04/map.go
+++ ch04/map.go
@@ -10,8 +10,13 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"os"
 	"sort"
+	"unicode"
+	"unicode/utf8"
 )
 
 func main() {
@@ -39,4 +44,46 @@ func main() {
 		fmt.Printf("k=%s : v=%d\t", name, ages[name])
 	}
 
+	fmt.Println()
+	countUnicode()
+}
+
+func countUnicode() {
+	// 字符数量
+	mapNum := make(map[rune]int)
+	// utf-8编码长度
+	var utflen [utf8.UTFMax + 1]int
+	// 无效字符数量
+	invNum := 0
+
+	inReader := bufio.NewReader(os.Stdin)
+	for {
+		// rune, nbytes, error
+		r, n, err := inReader.ReadRune()
+		if io.EOF == err {
+			break
+		}
+		if nil != err {
+			fmt.Fprintf(os.Stderr, "读取错误：%v\n", err)
+			os.Exit(1)
+		}
+		if 1 == n && unicode.ReplacementChar == r {
+			invNum++
+			continue
+		}
+		mapNum[r]++
+		utflen[n]++
+	}
+
+	fmt.Println("rune\t数量\n")
+	for c, n := range mapNum {
+		// %q: 该值对应的单引号括起来的go语法字符字面值，必要时会采用安全的转义表示
+		// %v:值的默认格式表示。当输出结构体时，扩展标志（%+v）会添加字段名
+		fmt.Printf("%q\t%d\n", c, n)
+	}
+	fmt.Println("长度\t数量\n")
+	for i, n := range utflen {
+		fmt.Printf("%d\t%d\n", i, n)
+	}
+	fmt.Printf("无效utf8字符数量:%d\n", invNum)
 }
