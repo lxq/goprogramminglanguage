0eeabbb4b2f9ba28072116e6359cb3ae9fdcd7af
lxq <lxq@weetgo.com>
2019-06-20 14:42:54 +0800

    增加输入参数选项.

--- ch08/spinner.go
+++ ch08/spinner.go
@@ -7,17 +7,22 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"time"
 )
 
+var n = flag.Int("n", 0, "输入要计算的第几个斐波那契数")
+
 func main() {
+	// 参数解析，必须!
+	flag.Parse()
+
 	// 输出表示主程序还在执行.
 	go spinner(100 * time.Microsecond)
 
-	const n = 45
-	fibN := fib(n)
-	fmt.Printf("\r第%d个斐波那契数是：%d\n", n, fibN)
+	fibN := fib(*n)
+	fmt.Printf("\r第%d个斐波那契数是：%d\n", *n, fibN)
 }
 
 // 定时输出
