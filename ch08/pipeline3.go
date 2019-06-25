826204742c3d9178caa9e59f2b595405d0621130
lxq <lxq@weetgo.com>
2019-06-25 12:19:50 +0800

    select example.

--- ch08/pipeline3.go
+++ ch08/pipeline3.go
@@ -19,6 +19,16 @@ func main() {
 	go square(natu, squa)
 	// printer(主goroutine)
 	printer(squa)
+
+	// select  P191
+	ch := make(chan int, 1)
+	for i := 0; i < 10; i++ {
+		select {
+		case x := <-ch:
+			fmt.Println(x) //0,2,4,6,8
+		case ch <- i:
+		}
+	}
 }
 
 // counter
